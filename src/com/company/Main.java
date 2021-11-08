e1e0dbfcecb5b1b8776bc3320302b771f2e853ff
noorkhan3226 <noorafghan3226@gmail.com>
2021-11-08 14:46:35 -0500

    3rd desk

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        desk deska=new desk();
+        deska.size=123;
+        deska.color="grey";
+
     }
 }
